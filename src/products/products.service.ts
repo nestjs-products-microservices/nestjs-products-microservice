dee09db328ed40a1e751c33fab2812ad56b2c91b
Leonardo Valeriano <leonardo.valeriano.public@gmail.com>
2025-10-08 00:32:47 -0500

    create products microservice

---
+++ src/products/products.service.ts
@@ -0,0 +1,80 @@
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from 'generated/prisma';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductService');
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto,
+    });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+    const total = await this.product.count({ where: { available: true } });
+    const lastPage = Math.ceil(total / limit);
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: {
+          available: true,
+        },
+      }),
+      meta: {
+        lastPage,
+        page,
+        total,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: {
+        id,
+        available: true,
+      },
+    });
+
+    if (!product)
+      throw new NotFoundException(`Product with id ${id} not found`);
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const { id: _, ...data } = updateProductDto;
+    await this.findOne(id);
+    return this.product.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+    return await this.product.update({
+      where: {
+        id,
+      },
+      data: {
+        available: false,
+      },
+    });
+  }
+}
