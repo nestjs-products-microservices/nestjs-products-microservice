0258432ce87a7d2cf567ca7bd8d7a0e380f59f60
Leonardo Valeriano <leonardo.valeriano.public@gmail.com>
2025-11-22 05:06:58 -0500

    validate id products exists

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -76,4 +76,25 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       },
     });
   }
+
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: 'Some products were not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
+  }
 }
